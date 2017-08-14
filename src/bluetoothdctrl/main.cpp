31c0887eb287c18a95dcc34f18028c93418daa06
Sandesh Ghimire <sandeshghimire@gmail.com>
2017-08-14 15:25:20 -0700

    Bluetoothctrl and Bluetoothlib

---
+++ src/bluetoothdctrl/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "../lib/evaBluetooth/Bluetooth.hpp"
+
+int main(int argc, char **argv)
+{
+    CoreBluetooth cb;
+    return 0;
+}
---
+++ src/lib/libBlueZ/Bluetooth.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sandesh Ghimire on 8/14/17.
+//
+
+#include "Bluetooth.hpp"
